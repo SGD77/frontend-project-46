ff96de49a68e2a3cef964a62009115fb1819f6e9
SGD77 <102750788+SGD77@users.noreply.github.com>
2024-09-09 11:38:00 +0300

    fix: step 2 edits

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,8 +4,10 @@ import { Command } from 'commander';
 const program = new Command();
 
 program
+    .name('gendiff')
     .description('Compares two configuration files and shows a difference.')
-    .option('-V, --version', 'output the version number')
+    .version('0.0.1', '-V, --version', 'output the version number')
     .helpOption('-h, --help', 'output usage information')
-    .option('-f, --format', 'output format')
+    .option('-f, --format <type>', 'output format')
+    .arguments('<filepath1> <filepath2>')
     .parse(process.arvg);
