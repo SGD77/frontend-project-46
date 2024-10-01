cc40bf45554d4051f7209320b96bd348acbeb589
SGD77 <102750788+SGD77@users.noreply.github.com>
2024-10-01 21:22:12 +0300

    fix: parseFile filename/path error

--- src/parseFile.js
+++ src/parseFile.js
@@ -1,11 +1,12 @@
-import * as path from 'path';
+// import * as path from 'path';
+import readFile from "./readFile.js";
 
 const parseFile = (file) => {
-    if (path.extname(file) === '.json') {
-        return JSON.parse(file);
-    }
+    // if (path.extname(file) === '.json') {
+        return JSON.parse(readFile(file));
+    // }
 }
 
 export default parseFile;
 
-// console.log(parseFile('./fixtures/file1.json'))
+// console.log(parseFile('../fixtures/file1.json'))
--- src/runComparison.js
+++ src/runComparison.js
@@ -1,14 +1,28 @@
 import compareContent from "./compareContent.js";
 import getFullPath from "./getFullPath.js";
-import readFile from "./readFile.js";
+// import readFile from "./readFile.js";
 import parseFile from "./parseFile.js";
 
 const runComparison = (file1, file2) => {
-const fileContent1 = readFile(getFullPath(file1));
-const fileContent2 = readFile(getFullPath(file2));
-const parsedContent1 = parseFile(fileContent1);
-const parsedContent2 = parseFile(fileContent2);
-return compareContent(parsedContent1, parsedContent2);
+    const parsedContent1 = parseFile(getFullPath(file1));
+    const parsedContent2 = parseFile(getFullPath(file2));
+    // const parsedContent2 = parseFile(fileContent2);
+    return compareContent(parsedContent1, parsedContent2);
 };
+
+// const f1 = {
+//     "host": "hexlet.io",
+//     "timeout": 50,
+//     "proxy": "123.234.53.22",
+//     "follow": false
+// };
+
+// const f2 = {
+//     "timeout": 20,
+//     "verbose": true,
+//     "host": "hexlet.io"
+// }
+
+console.log(runComparison('../fixtures/file1.json','../fixtures/file2.json'))
 
 export default runComparison;
