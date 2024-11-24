26de901106626440b2f8eccf1dd961cf0a8aa47b
SGD77 <102750788+SGD77@users.noreply.github.com>
2024-11-24 14:47:44 +0300

    refactor: 6 issue

--- __tests__/compare.test.js
+++ __tests__/compare.test.js
@@ -3,13 +3,13 @@ import { fileURLToPath } from 'url';
 import compare from '../src/runComparison.js';
 import { readFile } from '../src/utils.js';
 
-describe('output tests', () => {
-  const getPath = (filename) => {
-    const __filename = fileURLToPath(import.meta.url);
-    const __dirname = dirname(__filename);
-    return path.join(__dirname, '..', '__fixtures__', filename);
-  };
+const getPath = (filename) => {
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  return path.join(__dirname, '..', '__fixtures__', filename);
+};
 
+describe('output tests', () => {
   const json1Path = getPath('file1.json');
   const json2Path = getPath('file2.json');
   const yaml1Path = getPath('file1.yaml');
@@ -17,30 +17,27 @@ describe('output tests', () => {
   const stylishPath = getPath('stylish.ini');
   const plainPath = getPath('plain.ini');
   const jsonPath = getPath('json.ini');
-
-
-
   const stylish = readFile(stylishPath);
   const plain = readFile(plainPath);
   const json = readFile(jsonPath);
 
-  // test('stylish output', () => {
-  //   expect(compare(json1Path, json2Path, 'stylish')).toBe(stylish);
-  //   expect(compare(yaml1Path, yaml2Path, 'stylish')).toBe(stylish);
-  // });
+  test('stylish output', () => {
+    expect(compare(json1Path, json2Path, 'stylish')).toBe(stylish);
+    expect(compare(yaml1Path, yaml2Path, 'stylish')).toBe(stylish);
+  });
 
-  // test('default output', () => {
-  //   expect(compare(json1Path, json2Path)).toBe(stylish);
-  //   expect(compare(yaml1Path, yaml2Path)).toBe(stylish);
-  // });
+  test('default output', () => {
+    expect(compare(json1Path, json2Path)).toBe(stylish);
+    expect(compare(yaml1Path, yaml2Path)).toBe(stylish);
+  });
 
   test('plain output', () => {
     expect(compare(json1Path, json2Path, 'plain')).toBe(plain);
     expect(compare(yaml1Path, yaml2Path, 'plain')).toBe(plain);
   });
 
-  // test('json output', () => {
-  //   expect(compare(json1Path, json2Path, 'json')).toBe(json);
-  //   expect(compare(yaml1Path, yaml2Path, 'json')).toBe(json);
-  // });
+  test('json output', () => {
+    expect(compare(json1Path, json2Path, 'json')).toBe(json);
+    expect(compare(yaml1Path, yaml2Path, 'json')).toBe(json);
+  });
 });
