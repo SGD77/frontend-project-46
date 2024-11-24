675b693353688f211b28fb9d078b9bd827f8806a
SGD77 <102750788+SGD77@users.noreply.github.com>
2024-11-24 15:23:12 +0300

    refactor: 14 issue

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -8,33 +8,30 @@ const stylish = (diff) => {
     return `{\n${entries.join('\n')}\n${' '.repeat(currentLevel * 4)}}`;
   };
   const iter = (data, level) => {
-    const indent = ' '.repeat(level * 4 - 2);
     const result = data.map((item) => {
-      if (item.type === 'added') {
-        return `${indent}+ ${item.key}: ${printValue(item.value, level)}`;
+      const indent = ' '.repeat(level * 4 - 2);
+      switch (item.type) {
+        case 'added':
+          return `${indent}+ ${item.key}: ${printValue(item.value, level)}`;
+        case 'deleted':
+          return `${indent}- ${item.key}: ${printValue(item.value, level)}`;
+        case 'changed':
+          return [
+            `${indent}- ${item.key}: ${printValue(item.value1, level)}`,
+            `${indent}+ ${item.key}: ${printValue(item.value2, level)}`,
+          ].join('\n');
+        case 'nested':
+          return [
+            `${indent}  ${item.key}: {`,
+            iter(item.children, level + 1),
+            `${' '.repeat(level * 4)}}`,
+          ].join('\n');
+        case 'unchanged':
+          return `${indent}  ${item.key}: ${printValue(item.value, level)}`;
+        default:
+          return '';
       }
-      if (item.type === 'deleted') {
-        return `${indent}- ${item.key}: ${printValue(item.value, level)}`;
-      }
-      if (item.type === 'changed') {
-        return [
-          `${indent}- ${item.key}: ${printValue(item.value1, level)}`,
-          `${indent}+ ${item.key}: ${printValue(item.value2, level)}`,
-        ].join('\n');
-      }
-      if (item.type === 'nested') {
-        return [
-          `${indent}  ${item.key}: {`,
-          iter(item.children, level + 1),
-          `${' '.repeat(level * 4)}}`,
-        ].join('\n');
-      }
-      if (item.type === 'unchanged') {
-        return `${indent}  ${item.key}: ${printValue(item.value, level)}`;
-      }
-      return '';
     });
-
     return result.join('\n');
   };
   return `{\n${iter(diff, 1)}\n}`;
